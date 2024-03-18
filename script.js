55b8455c0fcd910f5dd1bfcd1f5f957259243f11
David Osose <144554752+Davidosose@users.noreply.github.com>
2024-03-19 00:01:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function showsidebar(){
+  const sidebar= document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+function hidesidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
+function showabout(){
+  const aboutdropdown= document.querySelector('#aboutdropdown')
+  aboutdropdown.style.display = 'flex'
+}
+function hideabout(){
+  const aboutdropdown= document.querySelector('#aboutdropdown')
+  aboutdropdown.style.display = 'none'
+}
+
+function showsupport(){
+  const supportdropdown = document.querySelector('#supportdropdown')
+  supportdropdown.style.display = 'flex'
+}
+function hidesupport(){
+  const supportdropdown = document.querySelector('#supportdropdown')
+  supportdropdown.style.display = 'none'
+}
